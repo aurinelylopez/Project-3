f3876b9c2ce03bbd6bed0c81a675d559fb2f3d9a
benjidurden <benjidurdenn@gmail.com>
2017-05-30 07:18:28 -0400

    placeholder questions

--- src/api/quiztime.js
+++ src/api/quiztime.js
@@ -15,28 +15,79 @@ var quizTime = [
               content: "Avril Lavigne"
           }
       ]
+  },
+  {
+      question: "Which music group has received the most Grammy Awards?",
+      answers: [
+          {
+              type: "FALSE",
+              content: "Foo Fighters"
+          },
+          {
+              type: "FALSE",
+              content: "Coldplay"
+          },
+          {
+              type: "TRUE",
+              content: "U2"
+          }
+      ]
+  },
+  {
+      question: "The Beatles' Abbey Road was originally going to be called Himalaya, and the album cover was slated to feature them in the Himalayas",
+      answers: [
+          {
+              type: "TRUE",
+              content: "TRUE"
+          },
+          {
+              type: "FALSE",
+              content: "FALSE"
+          },
+          {
+              type: "FALSE",
+              content: "Migos is better than The Beatles"
+          }
+      ]
+  },
+  {
+      question: "The band Gorillaz is a pun because a group of gorillas are known as a band",
+      answers: [
+          {
+              type: "TRUE",
+              content: "TRUE"
+          },
+          {
+              type: "FALSE",
+              content: "FALSE"
+          },
+          {
+              type: "FALSE",
+              content: "Not True, but Not False"
+          }
+      ]
+  },
+  {
+      question: "What is the name of the Seattle grunge band Nirvana''s first album, released in 1989?",
+      answers: [
+          {
+              type: "FALSE",
+              content: "In Utero"
+          },
+          {
+              type: "FALSE",
+              content: "Neverminid"
+          },
+          {
+              type: "TRUE",
+              content: "Bleach"
+          }
+      ]
   }
 ];
 
 export default quizTime;
 
-// ,
-//   {
-//       question: "Who is the lead singer for the American rock band Pearl Jam?",
-//       answers: [
-//           {
-//               type: "FALSE",
-//               content: "Chris Cornell'"
-//           },
-//           {
-//               type: "FALSE",
-//               content: "Eddie Van Halen"
-//           },
-//           {
-//               type: "TRUE",
-//               content: "Eddie Vedder"
-//           }
-//       ]
 //   },
 //   {
 //       question: "What is the name of the Seattle grunge band Nirvana''s first album, released in 1989?",
